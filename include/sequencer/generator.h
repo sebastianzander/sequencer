8fef62f713018d97c8145bbe044dfa010be30bec
Sebastian Zander <github@sebastianzander.de>
2022-01-16 18:33:20 +0100

    Fix incorrect call to generate() in inlined convenience method

--- include/sequencer/generator.h
+++ include/sequencer/generator.h
@@ -10,7 +10,7 @@ namespace sequencer_n
     sequence_t SEQUENCER_CPP_API generate(const std::vector<std::string> &description, const generatorContext_t &context);
     
     inline sequence_t generate(const std::string &description, const generatorContext_t &context) {
-        return generate({ description }, context);
+        return generate(std::vector<std::string> { description }, context);
     }
 }
 
